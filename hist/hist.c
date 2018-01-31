a2e055a21bdf078fd98d0707c2d92c18ac2141bf
fcovatti <fcovatti@gmail.com>
2018-01-31 16:03:32 -0200

    hist.c

--- hist/hist.c
+++ hist/hist.c
@@ -11,27 +11,21 @@
 #include <my_global.h>
 #include <mysql.h>
 #include <time.h>
-
 /*****************READ ME******************
  *
- *  Table sde must be created
+ *  Table sde and val_tr must be created
  *  Reference Table h0000_00 must be created
  *  Support to nponto values until 1 million (database)
  *  Writing events on receive (ignoring duplicate entries)
  *  Writing digital data and analog once a day or on persistent change in a 5 minute window
  * */
-
-#define CONVERT_GMT_TO_LOCAL	3
 #define QUERY_SIZE				100
 #define LONG_QUERY_SIZE			15000
 #define DATABASE_SIZE			1000000
 #define FLAGS_INIT				255
-
 #define LOG_MESSAGE(...)        do { print_time(log_file); fprintf(log_file, __VA_ARGS__); fflush(log_file); } while(false)
 
-//Log file
 static FILE * log_file = NULL;
-
 static int hist_socket_receive=0;
 static MYSQL *con;
 static char query[QUERY_SIZE];
@@ -93,7 +87,6 @@ static int create_db_comm(){
 		LOG_MESSAGE("%s\n", mysql_error(con));
 		return -1;
 	}  
-	//if (mysql_real_connect(con, "localhost", "user12", "34klq*", 
 	if (mysql_real_connect(con, "localhost", NULL, NULL, 
 				"bancotr", 0, NULL, 0) == NULL) 
 	{
@@ -119,7 +112,6 @@ static int check_packet(){
 		t = time(NULL);
 		tm = *localtime(&t);//get local time
 
-		
 		//first of all, if month has changed, or first time check/create a new table
 		if(tm.tm_mon!=currmon){
 			MYSQL_RES * result;
@@ -149,7 +141,6 @@ static int check_packet(){
 			currmon=tm.tm_mon;
 		}
 
-
 		//copy type of message
 		memcpy(&signature, msg_rcv, sizeof(unsigned int));
 
@@ -161,11 +152,6 @@ static int check_packet(){
 				info=(digital_w_time7_seq *)msg->info;
 				events_msgs++;
 
-				/*if(!(info->iq&0x01))
-					state=(info->iq&0xF7)|0x01; //bit 3 is invalid timestamp
-				else
-					state=(info->iq&0xF6); //bit 0 is to set the value
-				*/
 				state=(info->iq&0xF7);
 
 				//first insert into history if it has already been writen once (events are never writen)
@@ -178,16 +164,12 @@ static int check_packet(){
 						finish_with_error(con);
 						return -1;
 					}
-				
 				}
 
 				//always update val_tr
 				memset(query,0,QUERY_SIZE);
 				snprintf(query, QUERY_SIZE, "REPLACE INTO val_tr VALUES ('%d','%02d-%02d-%02d','%02d:%02d:%02d','%d','%d')", 
 					 msg->endereco, tm.tm_year+1900, tm.tm_mon+1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec,(state&0x01),state);
-				//printf("%s\n", query);
-				
-				//printf("iq %x state %x\n",info->iq,state);
 				
 				if (mysql_query(con,query)) {
 					finish_with_error(con);
@@ -196,28 +178,13 @@ static int check_packet(){
 
 				//than insert into sde
 				memset(query,0,QUERY_SIZE);
-#ifdef CONVERT_GMT_TO_LOCAL	
-				memset(&tm,0,sizeof(struct tm));
-				tm.tm_year=2000+info->ano-1900;
-				tm.tm_mon=info->mes;
-				tm.tm_mday=info->dia;
-				tm.tm_hour=info->hora;
-				t = mktime(&tm);
-				t=t+3600*CONVERT_GMT_TO_LOCAL;//define hours shift
-				tm=*localtime(&t);
-				//use new year/month/day/hour generate with shift. min and seconds continue the same
-				snprintf(query, QUERY_SIZE, "INSERT IGNORE INTO sde VALUES('%d','0','%02d-%02d-%02d','%02d:%02d:%02d', '%d', '%d', '0', NULL)", 
-						msg->endereco, tm.tm_year+1900, tm.tm_mon, tm.tm_mday, tm.tm_hour, info->min, info->ms/1000, info->ms%1000, state);
-#else
 				snprintf(query, QUERY_SIZE, "INSERT IGNORE INTO sde VALUES('%d','0','20%02d-%02d-%02d','%02d:%02d:%02d', '%d', '%d', '0', NULL)", 
 						msg->endereco, info->ano, info->mes, info->dia, info->hora, info->min, info->ms/1000, info->ms%1000, state);
-#endif
-				LOG_MESSAGE("%s\n", query);
+				//LOG_MESSAGE("%s\n", query);
 				if (mysql_query(con,query)) {
 					finish_with_error(con);
 					return -1;
 				}
-				
 			}	
 			else if(msg->tipo==1){
 				events_msgs++;	
@@ -225,7 +192,6 @@ static int check_packet(){
 			}
 			else{
 				error_msgs++;
-				//printf("wrong type %d\n",msg->tipo);
 			}
 		} //if it is a list of data
 		else if(signature==IHM_POINT_LIST_SIGN){
@@ -233,12 +199,6 @@ static int check_packet(){
 			msg_sq=(t_msgsupsq *)msg_rcv;
 			nponto=0;
 
-			//write data exactly every five minutos
-			//if(tm.tm_min%10>=5)
-			//	decminuto=5;
-			//else
-			//	decminuto=0;
-			
 			//new day or starting process, reset flags in order to write again all data (once a day)
 			if(tm.tm_mday!=currday){
 				for(i=0;i<DATABASE_SIZE;i++)
@@ -257,12 +217,6 @@ static int check_packet(){
 					memcpy(&nponto, &msg_sq->info[i*(sizeof(digital_seq)+sizeof(int))], sizeof(int));
 					info=(digital_seq *)&msg_sq->info[i*(sizeof(digital_seq)+sizeof(int))+sizeof(int)];
 
-					/*
-					if(!(info->iq&0x01))
-						state=(info->iq&0xF7)|0x01; //bit 3 is invalid timestamp
-					else
-						state=(info->iq&0xF6); //bit 0 is to set the value
-					*/
 					state=(info->iq&0xF7);
 
 					snprintf(longquery+strlen(longquery), QUERY_SIZE, "('%d','%02d-%02d-%02d','%02d:%02d:%02d','%d','%d'),", 
@@ -274,7 +228,6 @@ static int check_packet(){
 					return -1;
 				}
 
-
 				//than update history table
 				memset(longquery,0,LONG_QUERY_SIZE);
 				snprintf(longquery, QUERY_SIZE, "INSERT IGNORE INTO h%d_%02d (NPONTO, DATA, HORA, VALOR, FLAGS) VALUES ",tm.tm_year+1900, tm.tm_mon+1);
@@ -282,23 +235,12 @@ static int check_packet(){
 					memcpy(&nponto, &msg_sq->info[i*(sizeof(digital_seq)+sizeof(int))], sizeof(int));
 					info=(digital_seq *)&msg_sq->info[i*(sizeof(digital_seq)+sizeof(int))+sizeof(int)];
 					if(data[nponto].flags!=info->iq){
-						//snprintf(longquery+strlen(longquery), QUERY_SIZE, "('%d','%02d-%02d-%02d','%02d:%02d:00','0','%d'),", 
-						//nponto, tm.tm_year+1900, tm.tm_mon+1, tm.tm_mday, tm.tm_hour, (tm.tm_min/10)*10+decminuto, info->iq);
-						
 						state=(info->iq&0xF7);
-
-						/*if(!(info->iq&0x01))
-							state=(info->iq&0xF7)|0x01; //bit 3 is invalid timestamp
-						else
-							state=(info->iq&0xF6); //bit 0 is to set the value
-						*/
-
 						snprintf(longquery+strlen(longquery), QUERY_SIZE, "('%d','%02d-%02d-%02d','%02d:%02d:%02d','%d','%d'),", 
 							nponto, tm.tm_year+1900, tm.tm_mon+1, tm.tm_mday, tm.tm_hour, tm.tm_min,tm.tm_sec, (state&0x01), (state&0xFD));
 						insertone=1;
 						data[nponto].flags=info->iq;
 					}
-					
 				}
 				if(insertone){
 					longquery[strlen(longquery)-1]=';';
@@ -307,13 +249,11 @@ static int check_packet(){
 						return -1;
 					}
 				}
-				//printf("query %s \n", longquery);
 				digital_msgs++;	
 			
 			//if analog data
 			}else if(msg_sq->tipo==13){
 				flutuante_seq *info;
-			
 			
 				//first update val_tr
 				memset(longquery,0,LONG_QUERY_SIZE);
@@ -323,10 +263,6 @@ static int check_packet(){
 					memcpy(&nponto, &msg_sq->info[i*(sizeof(flutuante_seq)+sizeof(int))], sizeof(int));
 					info=(flutuante_seq *)&msg_sq->info[i*(sizeof(flutuante_seq)+sizeof(int))+sizeof(int)];
 	
-					//FIXME STATE
-			/*		if(!info->qds)
-						state=(info->qds|0x20);
-					*/
 					snprintf(longquery+strlen(longquery), QUERY_SIZE, "('%d','%02d-%02d-%02d','%02d:%02d:%02d','%.2f','%d'),", 
 						nponto, tm.tm_year+1900, tm.tm_mon+1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec,info->fr,info->qds);
 				}
@@ -336,32 +272,19 @@ static int check_packet(){
 					return -1;
 				}
 
-			
 				//than update history
 				memset(longquery,0,LONG_QUERY_SIZE);
 				snprintf(longquery, QUERY_SIZE, "INSERT IGNORE INTO h%d_%02d (NPONTO, DATA, HORA, VALOR, FLAGS) VALUES ",tm.tm_year+1900, tm.tm_mon+1);
 				for (i=0;i<msg_sq->numpoints;i++){
 					memcpy(&nponto, &msg_sq->info[i*(sizeof(flutuante_seq)+sizeof(int))], sizeof(int));
 					info=(flutuante_seq *)&msg_sq->info[i*(sizeof(flutuante_seq)+sizeof(int))+sizeof(int)];
-					//write analog if flag has changed
-					//otherwise write if values have changed more than 0.5% or at least 0.5 within a minute
+					//write analog if flag has changed otherwise write if values have changed more than 0.5% or 0.5 within a minute
 					//every five minutes write if value have changed more than 0.2% or at least 0.2
 					if(data[nponto].flags!=info->qds || 
 							((t-data[nponto].lastchange)>=60 && ((fabs(info->fr)>100 && (fabs(info->fr-data[nponto].value)/fabs(info->fr) > 0.005)) || 
 							(fabs(info->fr)<=100 && fabs(info->fr-data[nponto].value)>=0.5) )) ||
 						   	((t-data[nponto].lastchange)>=300 && ((fabs(info->fr)>100 && (fabs(info->fr-data[nponto].value)/fabs(info->fr) > 0.002)) || 
-							(fabs(info->fr)<=100 && fabs(info->fr-data[nponto].value)>=0.2) ))){ //&& (fabs(info->fr)>0.2 || fabs(data[nponto].value)>0.2))){
-
-							//((fabs(info->fr)>100 && (fabs(info->fr-data[nponto].value)/fabs(info->fr) > 0.005)) || 
-							//(fabs(info->fr)<=100 && fabs(info->fr-data[nponto].value)>=0.5)) ||
-							//
-						//snprintf(longquery+strlen(longquery), QUERY_SIZE, "('%d','%02d-%02d-%02d','%02d:%02d:00','%.2f','%d'),", 
-						//nponto, tm.tm_year+1900, tm.tm_mon+1, tm.tm_mday, tm.tm_hour, (tm.tm_min/10)*10+decminuto, info->fr, info->qds);
-						//snprintf(longquery+strlen(longquery), QUERY_SIZE, "('%d','%02d-%02d-%02d','%02d:%02d:%02d','%.2f','%d'),", 
-						//nponto, tm.tm_year+1900, tm.tm_mon+1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec, info->fr, info->qds);
-						//FIXME STATE
-						/*if(info->qds&0x01==0)
-							state=info->qds|0x20;*/
+							(fabs(info->fr)<=100 && fabs(info->fr-data[nponto].value)>=0.2) ))){ 
 						snprintf(longquery+strlen(longquery), QUERY_SIZE, "('%d','%02d-%02d-%02d','%02d:%02d:%02d','%.2f','%d'),", 
 						nponto, tm.tm_year+1900, tm.tm_mon+1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec, info->fr, info->qds);
 						insertone=1;
@@ -377,16 +300,13 @@ static int check_packet(){
 						return -1;
 					}
 				}
-				//printf("query %s \n", longquery);
 				analog_msgs++;
 			}else{
 				error_msgs++;
-				//printf("wrong gi type %d\n",msg_sq->tipo);
 			}
 		}
 		else{
 			error_msgs++;
-			//printf("wrong signature %d\n", signature);
 		}
 		free(msg_rcv);
 	}
@@ -401,16 +321,8 @@ static int open_log_file(){
 	struct tm now = *localtime(&t);
 	char flog[MAX_STR_NAME];
 
-#ifdef WIN32
-#ifdef WINE_TESTING
 	snprintf(flog,MAX_STR_NAME, "/tmp/hist_info-%04d%02d%02d%02d%02d.log", now.tm_year+1900, now.tm_mon+1, now.tm_mday,now.tm_hour, now.tm_min);
-#else
-	snprintf(flog,MAX_STR_NAME, "..\\logs\\hist_info-%04d%02d%02d%02d%02d.log", now.tm_year+1900, now.tm_mon+1, now.tm_mday,now.tm_hour, now.tm_min);
-#endif
-#else
-	snprintf(flog,MAX_STR_NAME, "/tmp/hist_info-%04d%02d%02d%02d%02d.log", now.tm_year+1900, now.tm_mon+1, now.tm_mday,now.tm_hour, now.tm_min);
-#endif
-
+	
 	log_file = fopen(flog, "w");
 	if(log_file==NULL){
 		printf("Error, cannot open log file %s\n",flog);
